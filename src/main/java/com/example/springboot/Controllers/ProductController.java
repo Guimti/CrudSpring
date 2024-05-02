783aa0cdf1bd6dadc2689b10262c8b7ac6670122
Guimti <guilhermeguimti@gmail.com>
2024-05-02 18:01:18 -0300

    Criação do método Put e Delete

--- src/main/java/com/example/springboot/Controllers/ProductController.java
+++ src/main/java/com/example/springboot/Controllers/ProductController.java
@@ -46,6 +46,56 @@ public class ProductController {
 
     }
 
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
+                                                @RequestBody @Valid ProductRecordDto productRecordDto) {
+
+        Optional<ProductModel> productO = producRepository.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        var productModel = productO.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(producRepository.save(productModel));
+
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productO = producRepository.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        producRepository.delete(productO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
