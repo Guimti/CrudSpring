4e1da9f691d810012c494b5e7d17dc191e9963c7
Guimti <guilhermeguimti@gmail.com>
2024-05-09 19:47:12 -0300

    Swagger OK

--- src/main/java/com/example/springboot/config/SwaggerConfig.java
+++ src/main/java/com/example/springboot/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.springboot.Controllers.ProductController"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.springboot.Controllers"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
