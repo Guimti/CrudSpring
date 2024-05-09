e49fc5178dd1f3f011f8fa6bd095c5536be4d6e2
Guimti <guilhermeguimti@gmail.com>
2024-05-09 01:18:18 -0300

    Implementando swagger, ERRO Failed to load API definition.                             Errors                             Hide
                                Fetch errorresponse status is 500 /v3/api-docs

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,28 +2,13 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.springboot.controllers")) // Pacote onde estão os controllers da sua aplicação
-				.paths(PathSelectors.any())
-				.build();
-	}
 
 }
